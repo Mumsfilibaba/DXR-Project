ac276a21a07758726daa5a0a51494f8241f52b98
Alexander Dahlin <alexander.dahlins@gmail.com>
2021-01-30 11:40:08 +0100

    Update EventDispatcher.h

--- DXR-Engine/Application/Events/EventDispatcher.h
+++ DXR-Engine/Application/Events/EventDispatcher.h
@@ -57,19 +57,19 @@ public:
     }
 
 public:
-    virtual void OnKeyReleased(EKey KeyCode, const ModifierKeyState& ModierKeyState)                override final;
-    virtual void OnKeyPressed(EKey KeyCode, Bool IsRepeat, const ModifierKeyState& ModierKeyState)  override final;
-    virtual void OnMouseMove(Int32 x, Int32 y)                                                      override final;
+    virtual void OnKeyReleased(EKey KeyCode, const ModifierKeyState& ModierKeyState) override final;
+    virtual void OnKeyPressed(EKey KeyCode, Bool IsRepeat, const ModifierKeyState& ModierKeyState) override final;
+    virtual void OnMouseMove(Int32 x, Int32 y) override final;
     virtual void OnMouseButtonReleased(EMouseButton Button, const ModifierKeyState& ModierKeyState) override final;
-    virtual void OnMouseButtonPressed(EMouseButton Button, const ModifierKeyState& ModierKeyState)  override final;
-    virtual void OnMouseScrolled(Float HorizontalDelta, Float VerticalDelta)                        override final;
-    virtual void OnCharacterInput(UInt32 Character)                                                 override final;
-    virtual void OnWindowResized(const TSharedRef<GenericWindow>& InWindow, UInt16 Width, UInt16 Height)    override final;
-    virtual void OnWindowMoved(const TSharedRef<GenericWindow>& Window, Int16 x, Int16 y)     override final;
+    virtual void OnMouseButtonPressed(EMouseButton Button, const ModifierKeyState& ModierKeyState) override final;
+    virtual void OnMouseScrolled(Float HorizontalDelta, Float VerticalDelta) override final;
+    virtual void OnCharacterInput(UInt32 Character) override final;
+    virtual void OnWindowResized(const TSharedRef<GenericWindow>& InWindow, UInt16 Width, UInt16 Height) override final;
+    virtual void OnWindowMoved(const TSharedRef<GenericWindow>& Window, Int16 x, Int16 y) override final;
     virtual void OnWindowFocusChanged(const TSharedRef<GenericWindow>& Window, Bool HasFocus) override final;
-    virtual void OnWindowMouseLeft(const TSharedRef<GenericWindow>& Window)                   override final;
-    virtual void OnWindowMouseEntered(const TSharedRef<GenericWindow>& Window)                override final;
-    virtual void OnWindowClosed(const TSharedRef<GenericWindow>& Window)                      override final;
+    virtual void OnWindowMouseLeft(const TSharedRef<GenericWindow>& Window) override final;
+    virtual void OnWindowMouseEntered(const TSharedRef<GenericWindow>& Window) override final;
+    virtual void OnWindowClosed(const TSharedRef<GenericWindow>& Window) override final;
 
 private:
     class GenericApplication* Application = nullptr;
