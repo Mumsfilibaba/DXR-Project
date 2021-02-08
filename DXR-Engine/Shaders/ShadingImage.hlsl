382f958f9e77411ea1e7fef9d4dd7894651367b4
Alexander Dahlin <alexander.dahlins@gmail.com>
2021-02-08 19:09:15 +0100

    Update ShadingImage.hlsl

--- DXR-Engine/Shaders/ShadingImage.hlsl
+++ DXR-Engine/Shaders/ShadingImage.hlsl
@@ -6,6 +6,24 @@ RWTexture2D<uint> Output : register(u0, space0);
 [numthreads(1, 1, 1)]
 void Main(ComputeShaderInput Input)
 {
-    uint2 TexCoord   = Input.DispatchThreadID.xy;
-    Output[TexCoord] = SHADING_RATE_4x4;
+    uint Width;
+    uint Height;
+    Output.GetDimensions(Width, Height);
+    
+    int2   TexCoord = int2(Input.DispatchThreadID.xy);
+    float2 Center   = float2(Width / 2, Height / 2);
+    float  Distance = length(float2(TexCoord) - Center);
+    
+    if (Distance > 50.0f)
+    {
+        Output[TexCoord] = SHADING_RATE_4x4;
+    }
+    else if (Distance > 25.0f)
+    {
+        Output[TexCoord] = SHADING_RATE_2x2;
+    }
+    else
+    {
+        Output[TexCoord] = SHADING_RATE_1x1;
+    }
 }
