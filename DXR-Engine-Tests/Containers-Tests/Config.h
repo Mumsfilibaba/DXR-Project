f04c67c0d426fd9272298cbe8173bbabe2c9c1fb
Alexander Dahlin <alexander.dahlins@gmail.com>
2021-09-03 23:22:29 +0200

    Update Config.h

--- DXR-Engine-Tests/Containers-Tests/Config.h
+++ DXR-Engine-Tests/Containers-Tests/Config.h
@@ -6,7 +6,7 @@
 
 /* Test Specific defines */
 #define RUN_TARRAY_TEST      (1)
-#define RUN_TSHAREDPTR_TEST  (1)
+#define RUN_TSHAREDPTR_TEST  (0)
 #define RUN_TFUNCTION_TEST   (1)
 #define RUN_DELEGATE_TEST    (1)
 #define RUN_TFIXEDARRAY_TEST (1)
