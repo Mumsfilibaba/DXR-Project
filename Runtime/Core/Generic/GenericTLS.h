400f77a1ac1e34c5f2aab0419547c5ea0c2e6ca5
Alexander Dahlin <alexander.dahlins@gmail.com>
2022-10-01 21:38:14 +0200

    Update GenericTLS.h

--- Runtime/Core/Generic/GenericTLS.h
+++ Runtime/Core/Generic/GenericTLS.h
@@ -15,20 +15,43 @@
 
 struct FGenericTLS
 {
+	/**
+	 * @return Returns the current thread ID
+	 */
 	static FORCEINLINE uint32 GetCurrentThreadID()
 	{
 		return TNumericLimits<uint32>::Min();
 	}
 
+	/**
+	 * @return Allocates and returns a new TLS slot
+	 */
 	static FORCEINLINE uint32 AllocTLSSlot()
 	{
 		return TNumericLimits<uint32>::Min();
 	}
 
+	/**
+	 * @brief Set an allocated TLS value
+	 * 
+	 * @param SlotIndex: Index to set
+	 * @param Value: Value to set
+	 */
 	static FORCEINLINE void SetTLSValue(uint32 SlotIndex, void* Value) { }
 
+	/**
+	 * @brief Retrieve the currently set TLS value
+	 * 
+	 * @param SlotIndex: Slot to retrieve the value for
+	 * @return Returns the current value
+	 */
 	static FORCEINLINE void* GetTLSValue(uint32 SlotIndex) { return nullptr; }
 
+	/**
+	 * @brief Free up the TLS slot
+	 * 
+	 * @param SlotIndex: Slot to free
+	 */
 	static FORCEINLINE void FreeTLSSlot(uint32 SlotIndex) { }
 };
 
