91627917867f6e96356cdb7ad49e6479d8255160
Alexander Dahlin <alexander.dahlins@gmail.com>
2023-11-26 10:04:48 +0100

    Update Renderer.cpp

--- Runtime/Renderer/Renderer.cpp
+++ Runtime/Renderer/Renderer.cpp
@@ -998,8 +998,6 @@ void FRenderer::Tick()
 
 void FRenderer::OnWindowResize(const FWindowEvent& Event)
 {
-    return;
-
     const uint32 Width  = Event.GetWidth();
     const uint32 Height = Event.GetHeight();
 
