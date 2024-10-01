af0cd723a6491b60ff2ffe409815b6f6d862ab2c
Mumsfilibaba <alexander@dahlins.nu>
2024-10-01 16:51:24 +0200

    Fixed HDR after moving tonemapping to seperate pass

--- Assets/Shaders/TemporalAA.hlsl
+++ Assets/Shaders/TemporalAA.hlsl
@@ -4,6 +4,8 @@
 #include "FilterFunction.hlsli"
 
 #define NUM_THREADS (16)
+#define FLT_EPS (0.00000001)
+#define HDR_CORRECTION (1)
 
 RWTexture2D<float4> FinalTarget : register(u0);
 RWTexture2D<float4> Output      : register(u1);
@@ -25,22 +27,33 @@ uint2 ClampToViewport(uint2 TexCoord, int2 Offset)
     return uint2(CurrentPosition);
 }
 
+float3 Tonemap(float3 x)
+{
+    // Reinhard tonemap
+    return x / (x + 1.0);
+}
+
+float3 InvTonemap(float3 x)
+{
+    return x / max(1.0 - x, FLT_EPS);
+}
+
 [numthreads(NUM_THREADS, NUM_THREADS, 1)]
 void Main(FComputeShaderInput Input)
 {
     const uint2 TexCoord = Input.DispatchThreadID.xy;
 
     // Sample neightbourhood and find the maxvalues
-    float3 SampleTotal  = Float3(0.0f);
-    float  SampleWeight = 0.0f;
+    float3 SampleTotal  = 0.0;
+    float  SampleWeight = 0.0;
 
     // Min and Max samples from this frame1
-    float3 MinSample = Float3( FLT32_MAX);
-    float3 MaxSample = Float3(-FLT32_MAX);
+    float3 MinSample = FLT32_MAX;
+    float3 MaxSample = -FLT32_MAX;
     
     // For variance calculation
-    float3 Moment0 = Float3(0.0f);
-    float3 Moment1 = Float3(0.0f);
+    float3 Moment0 = 0.0;
+    float3 Moment1 = 0.0;
 
     // Helpers
     float ClosestDepth = FLT32_MAX;
@@ -58,8 +71,13 @@ void Main(FComputeShaderInput Input)
             const float SubSampleWeight   = BlackmanHarrisFilter(SubSampleDistance);
 
             // Sample and ensure its a valid sample
+        #if HDR_CORRECTION
+            // Take HDR into account
+            float3 SubSample = Tonemap(FinalTarget[CurrentPosition].rgb);
+        #else
             float3 SubSample = FinalTarget[CurrentPosition].rgb;
-            SubSample = max(Float3(0.0f), SubSample);
+        #endif   
+            SubSample = max(0.0, SubSample);
 
             SampleTotal  += SubSample * SubSampleWeight;
             SampleWeight += SubSampleWeight;
@@ -79,11 +97,11 @@ void Main(FComputeShaderInput Input)
         }   
     }
 
-    const float3 CurrentSample = SampleTotal / SampleWeight;
-    
+    float3 CurrentSample = SampleTotal / SampleWeight;
+
     // Calculate and test the history-coordinate
-    const float2 MotionVector = VelocityBuffer[ClosestDepthPixelPosition].xy * float2(0.5f, -0.5f);
-    const float2 TexCoordUV   = (float2(TexCoord) + Float2(0.5f)) / float2(CameraBuffer.ViewportWidth, CameraBuffer.ViewportHeight);
+    const float2 MotionVector = VelocityBuffer[ClosestDepthPixelPosition].xy * float2(0.5, -0.5);
+    const float2 TexCoordUV   = (float2(TexCoord) + 0.5) / float2(CameraBuffer.ViewportWidth, CameraBuffer.ViewportHeight);
 
     const float2 HistoryTexCoord = TexCoordUV - MotionVector;
     if(any(HistoryTexCoord != saturate(HistoryTexCoord)))
@@ -94,8 +112,8 @@ void Main(FComputeShaderInput Input)
     }
 
     // Variance Calculation
-    const float OneDividedBySampleCount = 1.0f / 9.0f;
-    const float Gamma = 1.0f;
+    const float OneDividedBySampleCount = 1.0 / 9.0;
+    const float Gamma = 1.0;
 
     float3 MomentU  = Moment0 * OneDividedBySampleCount;
     float3 Sigma    = sqrt(abs((Moment1 * OneDividedBySampleCount) - (MomentU * MomentU)));
@@ -105,22 +123,34 @@ void Main(FComputeShaderInput Input)
     // TODO: Try and use a proper catmull rom sampling method using bilinear filtering
     // Clip and Clamp History sample to better fit the new frame
     float3 HistorySample = SampleTextureCatmullRom(HistoryBuffer, LinearSampler, HistoryTexCoord, float2(CameraBuffer.ViewportWidth, CameraBuffer.ViewportHeight));
+#if HDR_CORRECTION
+    HistorySample = Tonemap(HistorySample);
+#endif    
     HistorySample = clamp(HistorySample, MinSample, MaxSample);
     HistorySample = ClipAABB(MinColor, MaxColor, HistorySample);
 
-    const float3 CompressedSource  = CurrentSample * rcp(max(max(CurrentSample.r, CurrentSample.g), CurrentSample.b) + 1.0f);
-    const float3 CompressedHistory = HistorySample * rcp(max(max(HistorySample.r, HistorySample.g), HistorySample.b) + 1.0f);
+    const float3 CompressedSource  = CurrentSample * rcp(max(max(CurrentSample.r, CurrentSample.g), CurrentSample.b) + 1.0);
+    const float3 CompressedHistory = HistorySample * rcp(max(max(HistorySample.r, HistorySample.g), HistorySample.b) + 1.0);
     
     const float LuminanceSource  = Luminance(CompressedSource);
     const float LuminanceHistory = Luminance(CompressedHistory); 
     
     // Calculate weights
-    float SourceWeight  = 0.05f;
-    float HistoryWeight = 1.0f - SourceWeight;
-    SourceWeight  *= 1.0f / (1.0f + LuminanceSource);
-    HistoryWeight *= 1.0f / (1.0f + LuminanceHistory);
+#if HDR_CORRECTION
+    float SourceWeight  = 0.1;
+#else
+    float SourceWeight  = 0.05;
+#endif
 
-    const float3 NewSample = ((CurrentSample * SourceWeight) + (HistorySample * HistoryWeight)) / max(SourceWeight + HistoryWeight, 0.00001);
-    FinalTarget[TexCoord] = float4(NewSample, 1.0f);
-    Output[TexCoord]      = float4(NewSample, 1.0f);
+    float HistoryWeight = 1.0 - SourceWeight;
+    SourceWeight  *= 1.0 / (1.0 + LuminanceSource);
+    HistoryWeight *= 1.0 / (1.0 + LuminanceHistory);
+
+    float3 NewSample = ((CurrentSample * SourceWeight) + (HistorySample * HistoryWeight)) / max(SourceWeight + HistoryWeight, 0.00001);
+#if HDR_CORRECTION
+    NewSample = InvTonemap(NewSample);
+#endif
+
+    FinalTarget[TexCoord] = float4(NewSample, 1.0);
+    Output[TexCoord]      = float4(NewSample, 1.0);
 }
