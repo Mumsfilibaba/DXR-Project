44206d8305fad7adf3b800c97089713e33ab2165
Alexander Dahlin <alexander.dahlins@gmail.com>
2024-11-26 15:12:17 +0100

    Forgot to define common colors

--- Runtime/Core/Math/Color.cpp
+++ Runtime/Core/Math/Color.cpp
@@ -1,11 +1,11 @@
 #include "Core/Math/Color.h"
 
-const FFloatColor FFloatColor::Black;
+const FFloatColor FFloatColor::Black = FFloatColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-const FFloatColor FFloatColor::White;
+const FFloatColor FFloatColor::White = FFloatColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-const FFloatColor FFloatColor::Red;
+const FFloatColor FFloatColor::Red = FFloatColor(1.0f, 0.0f, 0.0f, 1.0f);
 
-const FFloatColor FFloatColor::Green;
+const FFloatColor FFloatColor::Green = FFloatColor(0.0f, 1.0f, 0.0f, 1.0f);
 
-const FFloatColor FFloatColor::Blue;
+const FFloatColor FFloatColor::Blue = FFloatColor(0.0f, 0.0f, 1.0f, 1.0f);
