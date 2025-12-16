9e2723d2ca306eab61454efd131728c7978886e5
Alexander Dahlin <alexander.dahlins@gmail.com>
2025-12-16 10:02:43 +0100

    Stylized the scene hierarchy

--- Runtime/Engine/EngineUI/Editor/EditorSceneHierarchyWidget.cpp
+++ Runtime/Engine/EngineUI/Editor/EditorSceneHierarchyWidget.cpp
@@ -2,7 +2,9 @@
 #include "Engine/EditorEngine.h"
 #include "ImGuiPlugin/ImGuiRenderer.h"
 #include "ImGuiPlugin/ImGuiExtensions.h"
+#include "Core/Misc/OutputDeviceLogger.h"
 #include <imgui.h>
+#include <imgui_internal.h>
 
 FEditorSceneHierarchyWidget::FEditorSceneHierarchyWidget(FEditorEngine* InEditorEngine)
     : EditorEngine(InEditorEngine)
@@ -52,6 +54,113 @@ void FEditorSceneHierarchyWidget::Draw()
 
 void FEditorSceneHierarchyWidget::DrawSceneInfo()
 {
+	const auto DrawFolderRow = [&](const char* Label, const char* Type, const char* OpenKey, bool bDefaultOpen, float IndentPx) -> bool
+	{
+		ImGuiStyle& Style = ImGui::GetStyle();
+
+		ImGui::PushID(OpenKey);
+
+		ImGuiStorage* Storage = ImGui::GetStateStorage();
+
+		const ImGuiID OpenId = ImGui::GetID("Open");
+		bool bOpen = Storage->GetBool(OpenId, bDefaultOpen);
+
+		const float SelectableRowHeight = ImGui::GetTextLineHeight() + Style.CellPadding.y * 2.0f;
+
+		ImGui::TableNextRow();
+		ImGui::TableSetColumnIndex(0);
+
+		const ImGuiSelectableFlags SelectableFlags =
+			ImGuiSelectableFlags_SpanAllColumns | 
+			ImGuiSelectableFlags_AllowItemOverlap;
+
+		if (ImGui::Selectable("##Row", false, SelectableFlags, ImVec2(0.0f, SelectableRowHeight)))
+		{
+			bOpen = !bOpen;
+			Storage->SetBool(OpenId, bOpen);
+		}
+
+		const ImVec2 RowMin       = ImGui::GetItemRectMin();
+		const ImVec2 RowMax       = ImGui::GetItemRectMax();
+		const float  RowHeight    = RowMax.y - RowMin.y;
+		const float  TextHeight   = ImGui::GetTextLineHeight();
+		const float  FontSize     = ImGui::GetFontSize();
+		const float  TextY        = RowMin.y + (RowHeight - TextHeight) * 0.5f;
+		const float  ArrowY       = RowMin.y + (RowHeight - FontSize) * 0.5f;
+		const float  ArrowTextGap = 6.0f;
+		const float  ArrowAdvance = FontSize;
+
+		ImGui::TableSetColumnIndex(0);
+		ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorScreenPos().x, TextY));
+		
+		// TODO: Draw icon here perhaps?
+
+		ImGui::TableSetColumnIndex(1);
+
+		const ImVec2   CollumnPosition = ImGui::GetCursorScreenPos();
+		const ImVec2   ArrowPosition   = ImVec2(CollumnPosition.x + IndentPx, ArrowY);
+		const ImU32    ArrowColor      = ImGui::GetColorU32(ImGuiCol_Text);
+		const ImGuiDir ArrowDirection  = bOpen ? ImGuiDir_Down : ImGuiDir_Right;
+
+		ImDrawList* DrawList = ImGui::GetWindowDrawList();
+		ImGui::RenderArrow(DrawList, ArrowPosition, ArrowColor, ArrowDirection, 1.0f);
+
+		ImGui::SetCursorScreenPos(ImVec2(ArrowPosition.x + ArrowAdvance + ArrowTextGap, TextY));
+		ImGui::TextUnformatted(Label);
+
+		ImGui::TableSetColumnIndex(2);
+		ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorScreenPos().x, TextY));
+		ImGui::TextUnformatted(Type);
+
+		ImGui::PopID();
+		return bOpen;
+	};
+
+	const auto DrawLeafRow = [&](const char* Label, const char* Type, bool bSelected, void* Id, float IndentPx, auto&& OnClick)
+	{
+		ImGuiStyle& Style = ImGui::GetStyle();
+
+		const float SelectableRowHeight = ImGui::GetTextLineHeight() + Style.CellPadding.y * 2.0f;
+
+		ImGui::TableNextRow();
+		ImGui::TableSetColumnIndex(0);
+
+		ImGui::PushID(Id);
+
+		const ImGuiSelectableFlags SelectableFlags =
+			ImGuiSelectableFlags_SpanAllColumns |
+			ImGuiSelectableFlags_AllowItemOverlap;
+
+		if (ImGui::Selectable("##Row", bSelected, SelectableFlags, ImVec2(0.0f, SelectableRowHeight)))
+		{
+			OnClick();
+		}
+
+		const ImVec2 RowMin       = ImGui::GetItemRectMin();
+		const ImVec2 RowMax       = ImGui::GetItemRectMax();
+		const float  RowHeight    = RowMax.y - RowMin.y;
+		const float  TextHeight   = ImGui::GetTextLineHeight();
+		const float  FontSize     = ImGui::GetFontSize();
+		const float  TextY        = RowMin.y + (RowHeight - TextHeight) * 0.5f;
+		const float  ArrowTextGap = 6.0f;
+		const float  ArrowAdvance = FontSize;
+
+		ImGui::TableSetColumnIndex(0);
+		ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorScreenPos().x, TextY));
+
+		ImGui::TableSetColumnIndex(1);
+
+		const ImVec2 Col1Pos = ImGui::GetCursorScreenPos();
+		ImGui::SetCursorScreenPos(ImVec2(Col1Pos.x + IndentPx + ArrowAdvance + ArrowTextGap, TextY));
+		ImGui::TextUnformatted(Label);
+
+		ImGui::TableSetColumnIndex(2);
+		ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorScreenPos().x, TextY));
+		ImGui::TextUnformatted(Type);
+
+		ImGui::PopID();
+	};
+
 	if (!EditorEngine)
 	{
 		ImGui::TextDisabled("No EditorEngine");
@@ -85,67 +194,6 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 	const bool bHasCameras  = Camera != nullptr;
 	const bool bHasLighting = bHasLights || bHasProbes;
 
-	const auto DrawLeafRow = [](const char* Label, const char* Type, const bool bSelected, void* Id, auto&& OnClick)
-	{
-		ImGui::TableNextRow();
-		ImGui::TableSetColumnIndex(0);
-
-		ImGui::PushID(Id);
-
-		ImGuiTreeNodeFlags Flags =
-			ImGuiTreeNodeFlags_Leaf |
-			ImGuiTreeNodeFlags_NoTreePushOnOpen |
-			ImGuiTreeNodeFlags_SpanFullWidth;
-
-		if (bSelected)
-		{
-			Flags |= ImGuiTreeNodeFlags_Selected;
-		}
-
-		ImGui::TreeNodeEx(Label, Flags);
-
-		if (ImGui::IsItemClicked(ImGuiMouseButton_Left))
-		{
-			OnClick();
-		}
-
-		ImGui::TableSetColumnIndex(1);
-		ImGui::TextUnformatted(Type);
-
-		ImGui::PopID();
-	};
-
-	const auto DrawFolderRowBegin = [](const char* Label, const char* Type, void* Id, bool bDefaultOpen = true) -> bool
-	{
-		ImGui::TableNextRow();
-		ImGui::TableSetColumnIndex(0);
-
-		ImGui::PushID(Id);
-
-		ImGuiTreeNodeFlags Flags = ImGuiTreeNodeFlags_SpanFullWidth;
-		if (bDefaultOpen)
-		{
-			Flags |= ImGuiTreeNodeFlags_DefaultOpen;
-		}
-
-		const bool bOpen = ImGui::TreeNodeEx(Label, Flags);
-
-		ImGui::TableSetColumnIndex(1);
-		ImGui::TextUnformatted(Type);
-
-		return bOpen;
-	};
-
-	const auto DrawFolderRowEnd = [&](bool bWasOpen)
-	{
-		if (bWasOpen)
-		{
-			ImGui::TreePop();
-		}
-
-		ImGui::PopID();
-	};
-
 	// -------------------------------------------------------------------------------------------
 	// Search Field (Actor Search)
 	// -------------------------------------------------------------------------------------------
@@ -171,9 +219,6 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 		DrawList->AddRect(ItemMin, ItemMax, BorderColor, BorderRounding, 0, BorderThickness);
 	}
 
-	// Separator between search and table
-	ImGui::Separator();
-
 	// -------------------------------------------------------------------------------------------
 	// Actor Table
 	// -------------------------------------------------------------------------------------------
@@ -186,24 +231,31 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 		ImGuiTableFlags_ScrollY |
 		ImGuiTableFlags_SizingStretchProp;
 
-	ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(10.0f, 6.0f));
+	ImGuiStyle& Style = ImGui::GetStyle();
+	ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(8.0f, 4.0f));
+
+	const float ChildIndent = Style.IndentSpacing;
 
 	const ImVec2 TableSize = ImVec2(0.0f, ImGui::GetContentRegionAvail().y);
-	if (!ImGui::BeginTable("##SceneOutliner", 2, TableFlags, TableSize))
+	if (!ImGui::BeginTable("##SceneOutliner", 3, TableFlags, TableSize))
 	{
 		ImGui::PopStyleVar();
 		return;
 	}
 
 	ImGui::TableSetupScrollFreeze(0, 1);
+	ImGui::TableSetupColumn("##Gutter", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_NoHeaderLabel | ImGuiTableColumnFlags_NoResize, 8.0f);
 	ImGui::TableSetupColumn("Item Label", ImGuiTableColumnFlags_WidthStretch);
-	ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, 120.0f);
+
+	const float TypeColWidth = ImGui::CalcTextSize("DirectionalLight").x + Style.CellPadding.x * 2.0f + 12.0f;
+	ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, TypeColWidth);
+
 	ImGui::TableHeadersRow();
 
 	// Cameras
 	if (bHasCameras)
 	{
-		const bool bCamerasOpen = DrawFolderRowBegin("Cameras", "Folder", (void*)"CamerasFolder", true);
+		const bool bCamerasOpen = DrawFolderRow("Cameras", "Folder", "CamerasFolder", true, 0.0f);
 		if (bCamerasOpen)
 		{
 			const CHAR* Name = "Main Camera";
@@ -221,20 +273,18 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 
 			if (bCameraFound)
 			{
-				DrawLeafRow(Name, "Camera", Camera == SelectedCamera, (void*)Camera, [&]()
+				DrawLeafRow("Main Camera", "Camera", Camera == SelectedCamera, (void*)Camera, ChildIndent, [&]()
 				{
 					EditorEngine->SetSelectedCamera(Camera);
 				});
 			}
 		}
-
-		DrawFolderRowEnd(bCamerasOpen);
 	}
 
 	// Actors folder
 	if (bHasActors)
 	{
-		const bool bActorsOpen = DrawFolderRowBegin("Actors", "Folder", (void*)"ActorsFolder", true);
+		const bool bActorsOpen = DrawFolderRow("Actors", "Folder", "ActorsFolder", true, 0.0f);
 		if (bActorsOpen)
 		{
 			for (FActor* Actor : Actors)
@@ -256,20 +306,18 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 				}
 
 				const char* Label = Name.IsEmpty() ? "Actor" : *Name;
-				DrawLeafRow(Label, "Actor", Actor == SelectedActor, (void*)Actor, [&]()
+				DrawLeafRow(Label, "Actor", Actor == SelectedActor, (void*)Actor, ChildIndent, [&]()
 				{
 					EditorEngine->SetSelectedActor(Actor);
 				});
 			}
 		}
-
-		DrawFolderRowEnd(bActorsOpen);
 	}
 
 	// Lighting folder
 	if (bHasLighting)
 	{
-		const bool bLightingOpen = DrawFolderRowBegin("Lighting", "Folder", (void*)"LightingFolder", true);
+		const bool bLightingOpen = DrawFolderRow("Lighting", "Folder", "LightingFolder", true, 0.0f);
 		if (bLightingOpen)
 		{
 			// Lights
@@ -306,7 +354,7 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 						}
 					}
 
-					DrawLeafRow(Label, TypeLabel, (Light == SelectedLight), (void*)Light, [&]()
+					DrawLeafRow(Label, TypeLabel, Light == SelectedLight, (void*)Light, ChildIndent, [&]()
 					{
 						EditorEngine->SetSelectedLight(Light);
 					});
@@ -337,15 +385,13 @@ void FEditorSceneHierarchyWidget::DrawSceneInfo()
 						}
 					}
 
-					DrawLeafRow(Label, "LightProbe", (Probe == SelectedLightProbe), (void*)Probe, [&]()
+					DrawLeafRow(Label, "LightProbe", Probe == SelectedLightProbe, (void*)Probe, ChildIndent, [&]()
 					{
 						EditorEngine->SetSelectedLightProbe(Probe);
 					});
 				}
 			}
 		}
-
-		DrawFolderRowEnd(bLightingOpen);
 	}
 
 	ImGui::EndTable();
